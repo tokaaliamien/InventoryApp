3a480437621001883de8752dea6d3a212168ab5c
tokaaliamien <tokaaliamien@example.com>
2018-01-26 03:40:13 +0200

    AddedProject

--- app/src/main/java/com/example/android/inventoryapp/AddActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/AddActivity.java
@@ -1,39 +1,52 @@
 package com.example.android.inventoryapp;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.android.inventoryapp.data.InventoryContract;
-import com.example.android.inventoryapp.data.InventoryDbHelper;
 
 import java.io.ByteArrayOutputStream;
-import java.sql.Blob;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AddActivity extends AppCompatActivity {
 
+    private final int CAMERA_REQUEST = 0;
+    private final int GALLERY_REQUEST = 1;
     private EditText nameEditText;
     private EditText quantityEditText;
     private EditText priceEditText;
     private EditText supplierEditText;
     private ImageView image;
     private ImageButton button;
-    Bitmap bitmap;
+    private int userChoosenMethod;
 
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,26 +57,38 @@ public class AddActivity extends AppCompatActivity {
         quantityEditText = (EditText) findViewById(R.id.quantity_edit_text);
         priceEditText = (EditText) findViewById(R.id.price_edit_text);
         supplierEditText = (EditText) findViewById(R.id.supplier_edit_text);
-
-        button = (ImageButton) findViewById(R.id.take_img_button);
         image = (ImageView) findViewById(R.id.img);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
-            }
-        });
-
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        bitmap = (Bitmap) data.getExtras().get("data");
-        image.setImageBitmap(bitmap);
-        button.setVisibility(View.GONE);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY_REQUEST)
+                onSelectGalleryResult(data);
+            else if (requestCode == CAMERA_REQUEST)
+                onSelectCameraReult(data);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectGalleryResult(Intent data) {
+        Bitmap bm = null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        image.setImageBitmap(bm);
+    }
+
+    private void onSelectCameraReult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        image.setImageBitmap(thumbnail);
     }
 
     @Override
@@ -75,8 +100,11 @@ public class AddActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_add:
+            case R.id.action_done:
                 insertProduct();
+                return true;
+            case R.id.action_add_photo:
+                selectImage();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -104,8 +132,6 @@ public class AddActivity extends AppCompatActivity {
             Toast.makeText(this, getString(R.string.negative_quantity), Toast.LENGTH_SHORT).show();
         else if ((!isEmailValid(supplier)) || supplier.isEmpty())
             Toast.makeText(this, getString(R.string.wrong_format_email), Toast.LENGTH_SHORT).show();
-        else if (bitmap == null)
-            Toast.makeText(this, getString(R.string.null_image), Toast.LENGTH_SHORT).show();
         else {
 
             ContentValues values = new ContentValues();
@@ -113,6 +139,14 @@ public class AddActivity extends AppCompatActivity {
             values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_QUANTITY, quantity);
             values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRICE, price);
             values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_SUPPLIER, supplier);
+
+            Bitmap bitmap;
+            BitmapDrawable drawable = (BitmapDrawable) image.getDrawable();
+            if (image.getDrawable() == null)
+                bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.no_photo);
+            else
+                bitmap = drawable.getBitmap();
+
 
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos);
@@ -132,10 +166,62 @@ public class AddActivity extends AppCompatActivity {
 
     }
 
-    public static boolean isEmailValid(String email) {
-        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
-        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+    private void selectImage() {
+        final String[] menuItems = {"Take Photo", "Choose from Gallary", "Canel"};
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Upload Photo");
+        builder.setItems(menuItems, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean permissionGranted = Utility.checkPermission(AddActivity.this);
+                switch (item) {
+                    case 0:
+                        if (permissionGranted)
+                            cameraIntent();
+                        break;
+                    case 1:
+                        if (permissionGranted)
+                            galleryIntent();
+                        break;
+                    case 2:
+                        dialog.dismiss();
+                        break;
+                }
+                userChoosenMethod = item;
+            }
+        });
+        builder.show();
+    }
+
+    private void cameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+
+    private void galleryIntent() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Photo"), GALLERY_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    switch (userChoosenMethod) {
+                        case 0:
+                            cameraIntent();
+                            break;
+                        case 1:
+                            galleryIntent();
+                            break;
+                    }
+                } else {
+                    Log.e("Add Actitivy", "permission denied");
+                }
+                break;
+        }
     }
 }
